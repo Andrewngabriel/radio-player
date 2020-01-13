829ed7765bfab3659b38267627565f3f7f374136
andrewngabriel <andrew@andrewtechful.com>
2020-01-13 01:25:27 -0500

    Implement favorites screen

--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -1,29 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:radio_player/models/radio_station.dart';
+import 'package:radio_player/models/station_list.dart';
 
 import '../utils/config.dart';
+import '../widgets/radio_card.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatefulWidget {
+  final Function selectStation;
+
+  FavoritesScreen({@required this.selectStation});
+
+  @override
+  _FavoritesScreenState createState() => _FavoritesScreenState();
+}
+
+class _FavoritesScreenState extends State<FavoritesScreen> {
+  StationList favoriteStations = StationList();
+  List<RadioStation> favoriteStationList = List<RadioStation>();
+
+  @override
+  void initState() {
+    super.initState();
+    this.favoriteStationList = favoriteStations.favoriteList;
+  }
+
+  @override
+  void didUpdateWidget(FavoritesScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("favorites ~ didUpdateWidget()");
+    updateStations();
+  }
+
+  void updateStations() async {
+    List<RadioStation> updatedStations =
+        await StationList.getRefreshedStations();
+    setState(() => this.favoriteStationList = updatedStations);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        title: Text(
-          "Favorites",
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10, 0.0),
-        decoration: Config.backgroundGradient(),
-        child: ListView(
-          children: <Widget>[
-            Text(
-              "Favorites!",
-              style: TextStyle(color: Colors.white, fontSize: 40),
-            ),
-          ],
-        ),
+    return Container(
+      decoration: Config.backgroundGradient(),
+      child: GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        padding: EdgeInsets.all(10.0),
+        children: favoriteStationList.map((station) {
+          int index = favoriteStationList.indexOf(station);
+          return RadioCard(
+            station.name,
+            station.frequency,
+            station.url,
+            index,
+            station.selected,
+            widget.selectStation,
+          );
+        }).toList(),
       ),
     );
   }
