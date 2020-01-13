3fffdd5a203ac7a829e19bcb6bb2579607a2ae72
andrewngabriel <andrew@andrewtechful.com>
2020-01-13 01:26:12 -0500

    Move stations list into its own screen widget

--- lib/screens/stations.dart
+++ lib/screens/stations.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 
+import '../models/radio_station.dart';
+import '../utils/config.dart';
+import '../widgets/radio_card.dart';
+
 class StationsScreen extends StatelessWidget {
+  final List<RadioStation> stations;
+  final Function selectStation;
+
+  StationsScreen({this.stations, this.selectStation});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: Config.backgroundGradient(),
+      child: GridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        padding: EdgeInsets.all(10.0),
+        children: this.stations.map((station) {
+          int index = this.stations.indexOf(station);
+          return RadioCard(
+            station.name,
+            station.frequency,
+            station.url,
+            index,
+            station.selected,
+            this.selectStation,
+          );
+        }).toList(),
+      ),
+    );
   }
 }
