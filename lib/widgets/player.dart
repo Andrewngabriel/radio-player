9e9668a7e2f0e893e3c98227f72ecf00ac0ceb80
andrewngabriel <andrew@andrewtechful.com>
2020-01-21 21:52:29 -0500

    Refactor Player widget in order to use the new player plugin

--- lib/widgets/player.dart
+++ lib/widgets/player.dart
@@ -1,26 +1,21 @@
 import 'dart:async';
-import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audiofileplayer/audio_system.dart';
+import 'package:audiofileplayer/audiofileplayer.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_media_notification/flutter_media_notification.dart';
 
 import '../models/radio_station.dart';
-import '../models/station_list.dart';
-import '../playerState.dart';
-import '../utils/station_favorites.dart';
 
 class Player extends StatefulWidget {
-  final String title;
-  final double freq;
-  final String url;
+  final RadioStation station;
   final int index;
 
   Player({
     Key key,
-    @required this.title,
-    @required this.freq,
-    @required this.url,
+    @required this.station,
     @required this.index,
   }) : super(key: key);
 
@@ -29,91 +24,112 @@ class Player extends StatefulWidget {
 }
 
 class _PlayerState extends State<Player> with SingleTickerProviderStateMixin {
-  StationFavorites _favorites = StationFavorites();
-  bool _isFavoriteStation;
+  static const String replayButtonId = 'replayButtonId';
+  static const String newReleasesButtonId = 'newReleasesButtonId';
 
   AnimationController _playBtnController;
-  AudioPlayer _audioPlayer;
-  AudioPlayerState _audioPlayerState;
-  Duration _duration;
-  Duration _position;
 
-  PlayerState _playerState = PlayerState.STOPPED;
-  StreamSubscription _durationSubscription;
-  StreamSubscription _positionSubscription;
-  StreamSubscription _playerCompleteSubscription;
-  StreamSubscription _playerErrorSubscription;
-  StreamSubscription _playerStateSubscription;
-
-  get _isPlaying => _playerState == PlayerState.PLAYING;
-  get _isPaused => _playerState == PlayerState.PAUSED;
+  Audio _remoteAudio;
+  bool _remoteAudioPlaying = false;
+  bool _remoteAudioLoading = false;
+  String _remoteErrorMessage;
 
   @override
   void initState() {
+    print('initState() called!');
     super.initState();
-    this._isFavoriteStation = false;
-    this._initAudioPlayer();
-    this._playBtnController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    MediaNotification.setListener('play', this._play);
-    MediaNotification.setListener('pause', this._pause);
+    AudioSystem.instance.addMediaEventListener(_mediaEventListener);
+    this._loadRemoteAudio();
   }
 
   @override
   void dispose() {
+    print('dispose() called!');
+    AudioSystem.instance.removeMediaEventListener(_mediaEventListener);
+    if (_remoteAudioPlaying) _remoteAudio.pause();
+    if (_remoteAudio != null) _remoteAudio.dispose();
     super.dispose();
-    this._audioPlayer.stop();
-    this._playerCompleteSubscription?.cancel();
-    this._playerErrorSubscription?.cancel();
-    this._playerStateSubscription?.cancel();
-    this._playBtnController.dispose();
   }
 
-  @override
-  void didUpdateWidget(Player oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    this._isFavoriteStation = this._isFavorite();
-    this._setURL();
+  void _loadRemoteAudio() {
+    _remoteErrorMessage = null;
+    _remoteAudioLoading = true;
+    _remoteAudio = Audio.loadFromRemoteUrl(
+      widget.station.url,
+      playInBackground: true,
+      onDuration: (duration) => setState(() => _remoteAudioLoading = false),
+      onError: (String message) => setState(() {
+        print("inside onError()");
+        print(message);
+        _remoteErrorMessage = message;
+//        _remoteAudio.dispose();
+//        _remoteAudio = null;
+        _remoteAudioPlaying = false;
+        _remoteAudioLoading = false;
+      }),
+    );
   }
 
-  void _setNotification() {
-    if (Platform.isIOS) {
-      _audioPlayer.setNotification(
-        title: widget.title,
-        artist: widget.freq.toString(),
-      );
-    }
+  Future<void> _resumeRemoteAudio() async {
+    _remoteAudio.resume();
+    setState(() => _remoteAudioPlaying = true);
+
+    final Uint8List imageBytes = await generateImageBytes();
+    AudioSystem.instance.setMetadata(AudioMetadata(
+      title: "Great title",
+      artist: "Great artist",
+      album: "Great album",
+      genre: "Great genre",
+      durationSeconds: 0,
+      artBytes: imageBytes,
+    ));
+
+    AudioSystem.instance.setAndroidNotificationButtons(<dynamic>[
+      AndroidMediaButtonType.pause,
+      AndroidMediaButtonType.stop,
+      const AndroidCustomMediaButton(
+          'replay', replayButtonId, 'ic_replay_black_36dp')
+    ], androidCompactIndices: <int>[
+      0
+    ]);
+
+    AudioSystem.instance.setSupportedMediaActions(<MediaActionType>{
+      MediaActionType.playPause,
+      MediaActionType.pause,
+    }, skipIntervalSeconds: 30);
   }
 
-  void _playPause() async {
-    if (_playerState == PlayerState.PLAYING) {
-      this._stop();
-      await this._playBtnController.forward().orCancel;
+  void _pauseRemoteAudio() {
+    _remoteAudio.pause();
+    setState(() => _remoteAudioPlaying = false);
+
+    AudioSystem.instance.setAndroidNotificationButtons(<dynamic>[
+      AndroidMediaButtonType.play,
+      AndroidMediaButtonType.stop,
+      const AndroidCustomMediaButton(
+          'new releases', newReleasesButtonId, 'ic_new_releases_black_36dp'),
+    ], androidCompactIndices: <int>[
+      0
+    ]);
+
+    AudioSystem.instance.setSupportedMediaActions(<MediaActionType>{
+      MediaActionType.playPause,
+      MediaActionType.play,
+    });
+  }
+
+  void _stopRemoteAudio() {
+    _remoteAudio.pause();
+    setState(() => _remoteAudioPlaying = false);
+    AudioSystem.instance.stopBackgroundDisplay();
+  }
+
+  void _playPause() {
+    if (_remoteAudioPlaying) {
+      this._pauseRemoteAudio();
     } else {
-      this._play();
-      await this._playBtnController.reverse().orCancel;
+      _remoteAudio.resume();
     }
-  }
-
-  void _setURL() async {
-    await _audioPlayer.setUrl(StationList.list[widget.index].url);
-  }
-
-  Future _favorite() async {
-    List<RadioStation> stations = StationList.list;
-    bool isInFavorites = this._favorites.isFavorite(stations[widget.index]);
-    if (isInFavorites) {
-      await this._favorites.removeFavorite(stations[widget.index]);
-    } else {
-      await this._favorites.addFavorite(stations[widget.index]);
-    }
-    this._isFavorite();
-  }
-
-  bool _isFavorite() {
-    bool status = _favorites.isFavorite(StationList.list[widget.index]);
-    setState(() => this._isFavoriteStation = status);
-    return status;
   }
 
   @override
@@ -125,10 +141,7 @@ class _PlayerState extends State<Player> with SingleTickerProviderStateMixin {
           begin: Alignment.bottomCenter,
           end: Alignment.topCenter,
           stops: [0.2, 1.0],
-          colors: [
-            Color(0xFF263241),
-            Color(0xFF413f6A),
-          ],
+          colors: [Color(0xFF263241), Color(0xFF413f6A)],
         ),
       ),
       child: Row(
@@ -137,30 +150,27 @@ class _PlayerState extends State<Player> with SingleTickerProviderStateMixin {
             child: Padding(
               padding: const EdgeInsets.only(top: 10.0),
               child: Text(
-                "${widget.title}, ${widget.freq}",
+                "${widget.station.name}, ${widget.station.frequency}",
                 style: TextStyle(color: Colors.white, fontSize: 20.0),
               ),
             ),
           ),
           IconButton(
-            icon: Icon(
-              (_isFavoriteStation) ? Icons.star : Icons.star_border,
-              color: Colors.orange,
-              size: 30,
-            ),
-            padding: EdgeInsets.only(top: 5, right: 20.0),
-            onPressed: this._favorite,
-          ),
-          IconButton(
-            icon: AnimatedIcon(
-              icon: (_playerState == PlayerState.LOADING)
-                  ? AnimatedIcons.close_menu
-                  : AnimatedIcons.pause_play,
-              progress: _playBtnController,
-            ),
+            icon: Icon(Icons.play_circle_outline),
             color: Colors.white,
             iconSize: 50,
             onPressed: this._playPause,
+            padding: EdgeInsets.only(right: 0.0),
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            color: Colors.white,
+            iconSize: 50,
+            onPressed: () {
+              print(_remoteAudio);
+              print("_remoteAudioPlaying: $_remoteAudioPlaying");
+              print("_remoteAudioLoading: $_remoteAudioLoading");
+            },
             padding: EdgeInsets.only(right: 0.0),
           ),
         ],
@@ -168,67 +178,63 @@ class _PlayerState extends State<Player> with SingleTickerProviderStateMixin {
     );
   }
 
-  void _initAudioPlayer() {
-    _audioPlayer = new AudioPlayer();
-
-    _playerCompleteSubscription =
-        _audioPlayer.onPlayerCompletion.listen((event) {
-      _onComplete();
-      setState(() => _position = _duration);
-    });
-
-    _playerErrorSubscription = _audioPlayer.onPlayerError.listen((msg) {
-      print('audioPlayer error : $msg');
-      setState(() {
-        _playerState = PlayerState.STOPPED;
-        _duration = Duration(seconds: 0);
-        _position = Duration(seconds: 0);
-      });
-    });
-
-    _audioPlayer.onPlayerStateChanged.listen((state) {
-      print(state);
-      if (!mounted) return;
-      setState(() => _audioPlayerState = state);
-    });
-  }
-
-  Future<int> _play() async {
-    final playPosition = (_position != null &&
-            _duration != null &&
-            _position.inMilliseconds > 0 &&
-            _position.inMilliseconds < _duration.inMilliseconds)
-        ? _position
-        : null;
-    final url = widget.url;
-    setState(() => _playerState = PlayerState.LOADING);
-    final result = await _audioPlayer.play(url, position: playPosition);
-    if (result == 1) setState(() => _playerState = PlayerState.PLAYING);
-
-    _audioPlayer.setPlaybackRate(playbackRate: 1.0);
-
-    return result;
-  }
-
-  Future<int> _pause() async {
-    final result = await _audioPlayer.pause();
-    if (result == 1) setState(() => _playerState = PlayerState.PAUSED);
-    return result;
-  }
-
-  Future<int> _stop() async {
-    final result = await _audioPlayer.stop();
-    if (result == 1) {
-      setState(() {
-        _playerState = PlayerState.STOPPED;
-        _position = Duration();
-      });
+  void _mediaEventListener(MediaEvent mediaEvent) {
+    print('App received media event of type: ${mediaEvent.type}');
+    final MediaActionType type = mediaEvent.type;
+    if (type == MediaActionType.play) {
+      _resumeRemoteAudio();
+    } else if (type == MediaActionType.pause) {
+      _pauseRemoteAudio();
+    } else if (type == MediaActionType.playPause) {
+      _remoteAudioPlaying ? _pauseRemoteAudio() : _resumeRemoteAudio();
+    } else if (type == MediaActionType.stop) {
+      _stopRemoteAudio();
+    } else if (type == MediaActionType.custom) {
+      if (mediaEvent.customEventId == replayButtonId) {
+        _remoteAudio.play();
+        AudioSystem.instance.setPlaybackState(true, 0.0);
+      } else if (mediaEvent.customEventId == newReleasesButtonId) {
+        print('New-releases button is not implemented in this exampe app.');
+      }
     }
-    MediaNotification.hideNotification();
-    return result;
   }
 
-  void _onComplete() {
-    setState(() => _playerState = PlayerState.STOPPED);
+  static Future<Uint8List> generateImageBytes() async {
+    // Random color generation methods: pick contrasting hues.
+    final Random random = Random();
+    final double bgHue = random.nextDouble() * 360;
+    final double fgHue = (bgHue + 180.0) % 360;
+    final HSLColor bgHslColor =
+        HSLColor.fromAHSL(1.0, bgHue, random.nextDouble() * .5 + .5, .5);
+    final HSLColor fgHslColor =
+        HSLColor.fromAHSL(1.0, fgHue, random.nextDouble() * .5 + .5, .5);
+
+    final Size size = const Size(200.0, 200.0);
+    final Offset center = const Offset(100.0, 100.0);
+    final ui.PictureRecorder recorder = ui.PictureRecorder();
+    final Rect rect = Offset.zero & size;
+    final Canvas canvas = Canvas(recorder, rect);
+    final Paint bgPaint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = bgHslColor.toColor();
+    final Paint fgPaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = fgHslColor.toColor()
+      ..strokeWidth = 8;
+    // Draw background color.
+    canvas.drawRect(rect, bgPaint);
+    // Draw 5 inset squares around the center.
+    for (int i = 0; i < 5; i++) {
+      canvas.drawRect(
+          Rect.fromCenter(center: center, width: i * 40.0, height: i * 40.0),
+          fgPaint);
+    }
+    // Render to image, then compress to PNG ByteData, then return as Uint8List.
+    final ui.Image image = await recorder
+        .endRecording()
+        .toImage(size.width.toInt(), size.height.toInt());
+    final ByteData encodedImageData =
+        await image.toByteData(format: ui.ImageByteFormat.png);
+    return encodedImageData.buffer.asUint8List();
   }
 }
