fd15e02ee8263a1dab1dd672896723d91ba6a8c9
andrewngabriel <andrew@andrewtechful.com>
2020-01-21 21:53:17 -0500

    Update Favorites Screen to use a builder in order to render the screen

--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -1,62 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:radio_player/models/radio_station.dart';
-import 'package:radio_player/models/station_list.dart';
+import 'package:radio_player/utils/station_favorites.dart';
 
+import '../models/radio_station.dart';
 import '../utils/config.dart';
 import '../widgets/radio_card.dart';
 
-class FavoritesScreen extends StatefulWidget {
+class FavoritesScreen extends StatelessWidget {
   final Function selectStation;
 
-  FavoritesScreen({@required this.selectStation});
-
-  @override
-  _FavoritesScreenState createState() => _FavoritesScreenState();
-}
-
-class _FavoritesScreenState extends State<FavoritesScreen> {
-  StationList favoriteStations = StationList();
-  List<RadioStation> favoriteStationList = List<RadioStation>();
-
-  @override
-  void initState() {
-    super.initState();
-    StationList.getRefreshedStations().then((stations) {
-      this.favoriteStationList = stations;
-    });
-  }
-
-  @override
-  void didUpdateWidget(FavoritesScreen oldWidget) {
-    updateStations();
-    super.didUpdateWidget(oldWidget);
-  }
-
-  void updateStations() {
-    StationList.getRefreshedStations().then((stations) {
-      setState(() => this.favoriteStationList = stations);
-    });
-  }
+  FavoritesScreen({this.selectStation});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: Config.backgroundGradient(),
-      child: ListView(
-        padding: EdgeInsets.all(10.0),
-        children: favoriteStationList.map((station) {
-          int index = favoriteStationList.indexOf(station);
-          return RadioCard(
-            station.id,
-            station.name,
-            station.frequency,
-            station.url,
-            index,
-            station.selected,
-            widget.selectStation,
-          );
-        }).toList(),
-      ),
+    return FutureBuilder<List<RadioStation>>(
+      future: StationFavorites().readAllFavorites(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        List<Widget> children;
+        List<RadioStation> favoriteStations = snapshot.data;
+
+        if (snapshot.hasData) {
+          children = favoriteStations.map((station) {
+            int index = favoriteStations.indexOf(station);
+            return RadioCard(
+              station.id,
+              station.name,
+              station.frequency,
+              station.url,
+              index,
+              station.selected,
+              this.selectStation,
+            );
+          }).toList();
+        } else if (snapshot.hasError) {
+          children = <Widget>[
+            Icon(Icons.error_outline, color: Colors.red, size: 60),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Error: ${snapshot.error}'),
+            )
+          ];
+        } else {
+          children = [
+            SizedBox(
+              child: CircularProgressIndicator(),
+              width: 60,
+              height: 60,
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Text('Awaiting result...'),
+            )
+          ];
+        }
+        return Container(
+          decoration: Config.backgroundGradient(),
+          child: ListView(padding: EdgeInsets.all(10.0), children: children),
+        );
+      },
     );
   }
 }
