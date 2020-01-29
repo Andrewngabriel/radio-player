7deb3fd82d69ef97b94af56915b6269b1ede8bf6
joshua-maros <jmaros512@gmail.com>
2020-01-28 16:41:26 -0800

    Favorite button is now in bottom bar.

--- lib/widgets/radio_card.dart
+++ lib/widgets/radio_card.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:radio_player/models/radio_station.dart';
-import 'package:radio_player/utils/favorites_storage.dart';
-import 'package:radio_player/utils/station_favorites.dart';
 
-class RadioCard extends StatefulWidget {
+class RadioCard extends StatelessWidget {
   final RadioStation station;
   final int index;
   final Function _selectStation;
@@ -15,68 +13,39 @@ class RadioCard extends StatefulWidget {
   );
 
   @override
-  _RadioCardState createState() {
-    return _RadioCardState();
-  }
-}
-
-class _RadioCardState extends State<RadioCard> {
-  @override
   Widget build(BuildContext context) {
     return OutlineButton(
-        onPressed: () => widget._selectStation(widget.station.id),
-        padding: EdgeInsets.all(0),
-        borderSide: (widget.station.selected)
-            ? BorderSide(color: Colors.blue, width: 2.0)
-            : BorderSide(color: Colors.black12),
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Text(
-                    widget.station.name,
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                  Text(
-                    widget.station.frequency.toString(),
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
+      onPressed: () => this._selectStation(this.station.id),
+      padding: EdgeInsets.all(0),
+      borderSide: (this.station.selected)
+          ? BorderSide(color: Colors.blue, width: 2.0)
+          : BorderSide(color: Colors.black12),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              this.station.name,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
               ),
             ),
-            Container(
-              padding: const EdgeInsets.all(0.0),
-              alignment: Alignment.bottomRight,
-              child: IconButton(
-                icon: Icon(
-                  StationFavorites().isFavorite(widget.station)
-                      ? Icons.favorite
-                      : Icons.favorite_border,
-                  color: Colors.white,
-                ),
-                onPressed: () => this.setState(() {
-                  if (StationFavorites().isFavorite(widget.station)) {
-                    StationFavorites().removeFavorite(widget.station);
-                  } else {
-                    StationFavorites().addFavorite(widget.station);
-                  }
-                }),
+            Text(
+              this.station.frequency.toString(),
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
               ),
-            )
+            ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
